305e93d99742d7e611fd3ba5189731b85204b477
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-05-01 22:35:59 +1000

    Modify HW 4

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -3,10 +3,11 @@ const paragraphs = document.getElementsByClassName('reveal');
 document.addEventListener('scroll', () => {
   for (paragraph of paragraphs) {
     const paragraphParameters = paragraph.getBoundingClientRect();
-    
-    if (paragraphParameters.top < window.innerHeight - paragraphParameters.height) {
+
+    if (paragraphParameters.top < window.innerHeight && paragraphParameters.top > -paragraphParameters.height) {
       paragraph.classList.add('reveal_active');
+    } else {
+      paragraph.classList.remove('reveal_active');
     }
   }
 })
-
